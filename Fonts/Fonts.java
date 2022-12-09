b3513175b572a7d0d92921570254a5d140ef03ce
Monte Ho <lokhang.ho@mail.utoronto.ca>
2022-12-09 15:56:53 -0500

    factory design pattern

---
+++ Fonts/Fonts.java
@@ -0,0 +1,12 @@
+package Fonts;
+
+public class Fonts {
+    private String name;
+
+    public Fonts(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
