6297bd586b638dbb27bb2b1b2aa6367988e27f93
Monte Ho <lokhang.ho@mail.utoronto.ca>
2022-12-10 12:41:37 -0500

    javadoc comments

--- FontFactory.java
+++ FontFactory.java
@@ -3,7 +3,11 @@ import Fonts.*;
 import java.util.Objects;
 
 public class FontFactory{
-
+    /**
+     * Create different fonts
+     * @param product
+     * @return font of type product
+     */
     public Fonts createFont(String product){
         if(Objects.equals(product, "Arial")) return new Arial();
         if(Objects.equals(product, "Courier")) return new Courier();
